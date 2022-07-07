7562ecf0454d92cc4aeb9b0d2cff6e5caf07edae
JoseSunoj <l00162972@student.lyit.ie>
2022-07-07 21:40:19 +0100

    Initial commit
    source code, actions scripts, etc.

---
+++ src/main/java/com/jhipster/ecommerce/product/config/LoggingAspectConfiguration.java
@@ -0,0 +1,17 @@
+package com.jhipster.ecommerce.product.config;
+
+import com.jhipster.ecommerce.product.aop.logging.LoggingAspect;
+import org.springframework.context.annotation.*;
+import org.springframework.core.env.Environment;
+import tech.jhipster.config.JHipsterConstants;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class LoggingAspectConfiguration {
+
+    @Bean
+    @Profile(JHipsterConstants.SPRING_PROFILE_DEVELOPMENT)
+    public LoggingAspect loggingAspect(Environment env) {
+        return new LoggingAspect(env);
+    }
+}
---
+++ src/main/java/com/jhipster/ecommerce/product/domain/enumeration/Size.java
@@ -0,0 +1,12 @@
+package com.jhipster.ecommerce.product.domain.enumeration;
+
+/**
+ * The Size enumeration.
+ */
+public enum Size {
+    S,
+    M,
+    L,
+    XL,
+    XXL,
+}
